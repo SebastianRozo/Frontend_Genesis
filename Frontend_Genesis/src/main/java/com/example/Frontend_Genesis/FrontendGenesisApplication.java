2f5504de7d81ec20b5e9735e51ffc0a56e4ac689
SebastianRozo <rozosebastian02@gmail.com>
2025-05-07 22:58:44 -0500

    FEAT: Upload a first commit

---
+++ Frontend_Genesis/src/main/java/com/example/Frontend_Genesis/FrontendGenesisApplication.java
@@ -0,0 +1,13 @@
+package com.example.Frontend_Genesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendGenesisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendGenesisApplication.class, args);
+	}
+
+}
---
+++ Frontend_Genesis/src/test/java/com/example/Frontend_Genesis/FrontendGenesisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Frontend_Genesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendGenesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
